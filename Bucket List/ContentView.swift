032c8488bef2d48dfeb10cf7c82aaf95eb8463a0
Resn4R <purview-joust.0x@icloud.com>
2023-11-07 13:24:37 +0000

    exploring conformance to protocols pt. 2

--- Bucket List/ContentView.swift
+++ Bucket List/ContentView.swift
@@ -7,7 +7,19 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ]
+    
     let values = [1,4,6,7,2,8,9].sorted()
     
     var body: some View {
