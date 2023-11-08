eac6f7f7a4707402d58aa1d69bb77a53bcdbb5fd
Resn4R <purview-joust.0x@icloud.com>
2023-11-08 11:04:27 +0000

    refactoring for deployment target iOS 17

--- Bucket List/ContentView.swift
+++ Bucket List/ContentView.swift
@@ -14,22 +14,21 @@ struct Location: Identifiable {
 }
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    private var cameraPosition: MapCameraPosition = .camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), distance: 100_000))
 
     let locations = [
         Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
         Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
     ]
+    
     var body: some View {
         NavigationStack{
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink { Text(location.name) }{
-                        Circle()
-                            .stroke(.red, lineWidth: 3)
-                            .frame(width: 44, height: 44)
+            Map(initialPosition: cameraPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate, anchor: .center) {
+                        Image(systemName: "mappin")
                             .onTapGesture {
-                                print("tapped on location \(location.name)")
+                                print("Tapped on \(location.name)")
                             }
                     }
                 }
