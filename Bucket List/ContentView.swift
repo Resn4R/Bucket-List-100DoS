05e38910d6fe20d10ba9291bfedd5e3a420348c2
Resn4R <purview-joust.0x@icloud.com>
2023-11-08 10:28:02 +0000

    implementing MapKit pt.1

--- Bucket List/ContentView.swift
+++ Bucket List/ContentView.swift
@@ -4,49 +4,15 @@
 //
 //  Created by Vito Borghi on 06/11/2023.
 //
-
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
 struct ContentView: View {
-    var loadingState = LoadingState.loading
-    var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
-        }
-    }
-}
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
 
-struct LoadingView: View {
     var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed!")
-    }
-}
-
-extension FileManager {
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+        Map(coordinateRegion: $mapRegion)
+            
     }
 }
 
